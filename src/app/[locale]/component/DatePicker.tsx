5af610e53b8324630f1121b926ed62b6e8389f37
rei <lamduchoa96@gmail.com>
2023-06-25 23:58:59 +0700

    date picker

--- src/app/[locale]/component/DatePicker.tsx
+++ src/app/[locale]/component/DatePicker.tsx
@@ -2,14 +2,55 @@
 import React, { useState } from "react";
 
 import ReactDatePicker from "react-datepicker";
+import getMonth from "date-fns/getMonth";
+import { getYear } from "date-fns";
 
 export interface DatePickerProps {}
 
 export function DatePicker(props: DatePickerProps) {
   const [dateRange, setDateRange] = useState([null, null]);
   const [startDate, endDate] = dateRange;
+  const months = [
+    "Tháng Một",
+    "Tháng Hai",
+    "Tháng Ba",
+    "Tháng Tư",
+    "Tháng Năm",
+    "Tháng Sáu",
+    "Tháng Bảy",
+    "Tháng Tám",
+    "Tháng Chín",
+    "Tháng Mười ",
+    "Tháng Mười Một ",
+    "Tháng Mười Hai ",
+  ];
   return (
     <ReactDatePicker
+      renderCustomHeader={({
+        date,
+        changeYear,
+        changeMonth,
+        decreaseMonth,
+        increaseMonth,
+        prevMonthButtonDisabled,
+        nextMonthButtonDisabled,
+      }) => (
+        <div className="Calendar-header d-flex">
+          <button onClick={decreaseMonth} disabled={prevMonthButtonDisabled}>
+            {"<"}
+          </button>
+
+          <span>{months[getMonth(date)] + " " + getYear(date)}</span>
+
+          <button onClick={increaseMonth} disabled={nextMonthButtonDisabled}>
+            {">"}
+          </button>
+        </div>
+      )}
+      formatWeekDay={(nameOfDay) => {
+        console.log(nameOfDay);
+        return nameOfDay.slice(0, 3);
+      }}
       selectsRange={true}
       startDate={startDate}
       endDate={endDate}
--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -1,106 +1,132 @@
 "use client";
+
 import { createGlobalStyle } from "styled-components";
 
-export const GlobalStyle = createGlobalStyle`
+export const GlobalStyle = createGlobalStyle` @font-face {
+  font-family: 'Sun World';
+  src: url('/static/fonts/SunWorld-Regular.eot');
+  src: url('/static/fonts/SunWorld-Regular.eot?#iefix') format('embedded-opentype'),
+    url('/static/fonts/SunWorld-Regular.woff2') format('woff2'),
+    url('/static/fonts/SunWorld-Regular.woff') format('woff'),
+    url('/static/fonts/SunWorld-Regular.ttf') format('truetype'),
+    url('/static/fonts/SunWorld-Regular.svg#SunWorld-Regular') format('svg');
+  font-weight: 400;
+  font-style: normal;
+  font-display: swap;
+}
 
 @font-face {
-    font-family: 'Sun World';
-    src: url('/static/fonts/SunWorld-Regular.eot');
-    src: url('/static/fonts/SunWorld-Regular.eot?#iefix') format('embedded-opentype'),
-      url('/static/fonts/SunWorld-Regular.woff2') format('woff2'),
-      url('/static/fonts/SunWorld-Regular.woff') format('woff'),
-      url('/static/fonts/SunWorld-Regular.ttf') format('truetype'),
-      url('/static/fonts/SunWorld-Regular.svg#SunWorld-Regular') format('svg');
-    font-weight: 400;
-    font-style: normal;
-    font-display: swap;
-  }
+  font-family: 'Sun World';
+  src: url('/static/fonts/SunWorld-Bold.eot');
+  src: url('/static/fonts/SunWorld-Bold.eot?#iefix') format('embedded-opentype'),
+    url('/static/fonts/SunWorld-Bold.woff2') format('woff2'),
+    url('/static/fonts/SunWorld-Bold.woff') format('woff'),
+    url('/static/fonts/SunWorld-Bold.ttf') format('truetype'),
+    url('/static/fonts/SunWorld-Bold.svg#SunWorld-Bold') format('svg');
+  font-weight: 900;
+  font-style: normal;
+  font-display: swap;
+}
 
-  @font-face {
-    font-family: 'Sun World';
-    src: url('/static/fonts/SunWorld-Bold.eot');
-    src: url('/static/fonts/SunWorld-Bold.eot?#iefix') format('embedded-opentype'),
-      url('/static/fonts/SunWorld-Bold.woff2') format('woff2'),
-      url('/static/fonts/SunWorld-Bold.woff') format('woff'),
-      url('/static/fonts/SunWorld-Bold.ttf') format('truetype'),
-      url('/static/fonts/SunWorld-Bold.svg#SunWorld-Bold') format('svg');
-    font-weight: 900;
-    font-style: normal;
-    font-display: swap;
-  }
+@font-face {
+  font-family: 'Sun World';
+  src: url('/static/fonts/SunWorld-Light.eot');
+  src: url('/static/fonts/SunWorld-Light.eot?#iefix') format('embedded-opentype'),
+    url('/static/fonts/SunWorld-Light.woff2') format('woff2'),
+    url('/static/fonts/SunWorld-Light.woff') format('woff'),
+    url('/static/fonts/SunWorld-Light.ttf') format('truetype'),
+    url('/static/fonts/SunWorld-Light.svg#SunWorld-Light') format('svg');
+  font-weight: 300;
+  font-style: normal;
+  font-display: swap;
+}
 
-  @font-face {
-    font-family: 'Sun World';
-    src: url('/static/fonts/SunWorld-Light.eot');
-    src: url('/static/fonts/SunWorld-Light.eot?#iefix') format('embedded-opentype'),
-      url('/static/fonts/SunWorld-Light.woff2') format('woff2'),
-      url('/static/fonts/SunWorld-Light.woff') format('woff'),
-      url('/static/fonts/SunWorld-Light.ttf') format('truetype'),
-      url('/static/fonts/SunWorld-Light.svg#SunWorld-Light') format('svg');
-    font-weight: 300;
-    font-style: normal;
-    font-display: swap;
-  }
+@font-face {
+  font-family: 'Sun World';
+  src: url('/static/fonts/SunWorld-Heavy.eot');
+  src: url('/static/fonts/SunWorld-Heavy.eot?#iefix') format('embedded-opentype'),
+    url('/static/fonts/SunWorld-Heavy.woff2') format('woff2'),
+    url('/static/fonts/SunWorld-Heavy.woff') format('woff'),
+    url('/static/fonts/SunWorld-Heavy.ttf') format('truetype'),
+    url('/static/fonts/SunWorld-Heavy.svg#SunWorld-Heavy') format('svg');
+  font-weight: 600;
+  font-style: normal;
+  font-display: swap;
+}
 
-  @font-face {
-    font-family: 'Sun World';
-    src: url('/static/fonts/SunWorld-Heavy.eot');
-    src: url('/static/fonts/SunWorld-Heavy.eot?#iefix') format('embedded-opentype'),
-      url('/static/fonts/SunWorld-Heavy.woff2') format('woff2'),
-      url('/static/fonts/SunWorld-Heavy.woff') format('woff'),
-      url('/static/fonts/SunWorld-Heavy.ttf') format('truetype'),
-      url('/static/fonts/SunWorld-Heavy.svg#SunWorld-Heavy') format('svg');
-    font-weight: 600;
-    font-style: normal;
-    font-display: swap;
-  }
+.--link {
+  cursor: pointer;
+}
 
-  .--link {
-    cursor: pointer;
-  }
+:root {
+  --color-1: #EA2127;
+  --color-2: #F2F2F2;
+  --color-3: #2C2C2C;
+  --color-4: #F9B217;
+  --color-5: #8F2623;
+  --color-6: #F6DB5F;
+  --color-7: #852221;
+  --color-8: #B1B1B1;
+  --font-body: 'Sun World';
+  --transition-regular: all 0.15s ease-out;
+  --transition-smooth: all 0.5s cubic-bezier(0.04, 1, 0.6, 0.97);
+}
 
-  :root {
-    --color-1: #EA2127;
-    --color-2: #F2F2F2;
-    --color-3: #2C2C2C;
-    --color-4: #F9B217;
-    --color-5: #8F2623;
-    --color-6: #F6DB5F;
-    --color-7: #852221;
-    --color-8: #B1B1B1;
-    --font-body: 'Sun World';
-    --transition-regular: all 0.15s ease-out;
-    --transition-smooth: all 0.5s cubic-bezier(0.04, 1, 0.6, 0.97);
-  }
+body {
+  font-size: 16px;
+  font-family: var(--font-body) !important;
+  color: var(--color-3);
+  line-height: 20px;
+  background: #f5f5f55c;
 
-  body {
-    font-size: 16px;
-    font-family: var(--font-body) !important;
-    color: var(--color-3);
-    line-height: 20px;
-    background: #f5f5f55c;
-
-    @media (max-width: 768px) {
-      font-size: 14px !important;
-    }
+  @media (max-width: 768px) {
+    font-size: 14px !important;
   }
+}
+
 h1 {
-    color: red
+  color: red
 }
+
 .react-datepicker {
-  font-family:var(--font-body);
-  font-size:18px;
- 
+  font-family: var(--font-body);
+  font-size: 18px;
+  width: 346px;
+  height: 327px;
+
+  display: flex;
+  padding: 0 20px;
+
+  .react-datepicker__month-container {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .react-datepicker__header {
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    border: none;
+    padding: 0;
+    border-radius: 0;
+    background-color: transparent;
+  }
+
+  .react-datepicker__day {
+    margin: 5px 0;
+    height: 24px;
+    width: 24px;
+    line-height: 24px;
+  }
+
+  .react-datepicker__day--in-range {
+    color: #2C2C2C;
+    border-radius: 0;
+    background-color: #F7A7A9;
+  }
 }
-.react-datepicker__day{
-  margin:5px 0;
-  height: 24px;
-  width: 24px;
-  line-height: 24px;
-}
-.react-datepicker__day--in-range{
-  color: #2C2C2C;
-  border-radius: 0;
-  background-color:#F7A7A9;
-}
+
+
 `;
